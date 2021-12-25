8bcf3a8488a122a1879887699dc6007e3bcbf9c8
Zeya LT <zeya.zlt@gmail.com>
2021-12-25 10:54:08 +0800

    changed to matplotplib fig

--- app.py
+++ app.py
@@ -2,6 +2,7 @@
 from parkwhere import extract_all_features
 import streamlit as st
 import datetime as dt
+import matplotlib.pyplot as plt
 import pandas as pd
 import pickle
 import plotly.graph_objects as go
@@ -56,11 +57,11 @@ def main():
     p1, p2 = st.columns([1, 1])
 
     # Get date and time inputs (defaults to current date and time)
-    date_predict = p1.date_input('Select date', dt.date.today(), key='1') 
+    date_predict = p1.date_input('Enter date...', dt.date.today(), key='1') 
     time_now_predict = dt.datetime.now() + dt.timedelta(hours=8) # Based on server time
     # time_now_predict = dt.datetime.now() # Based on local machine time
     time_now_predict = time_now_predict.strftime("%H:%M")
-    time_now_predict = p2.text_input("Enter time", value=time_now_predict, key='1') 
+    time_now_predict = p2.text_input("... and time", value=time_now_predict, key='1') 
 
     # Load trained model
     model = pickle.load(open('model/model.sav', 'rb'))
@@ -74,25 +75,29 @@ def main():
     # Generate prediction and predicted probabilities
     pred = model.predict(predict_df)[0]
     pred_proba = model.predict_proba(predict_df)[0]
-
+    
     # Create a donut chart 
-    pred_fig_pie = go.Figure()
-    pred_fig_pie.add_trace(go.Pie(labels=model.classes_, values=pred_proba, hole=0.7))
-    # pred_fig_pie.update_layout(autosize=False, width=400, height=400, margin=dict(l=10, r=10, b=10, t=10, pad=0))
+    labels = [i+"\n"+str(round(j,3)) for i, j in zip(model.classes_, pred_proba)]
+    plt.figure(figsize=(6,6))
+    fig = plt.pie(pred_proba, labels=labels, colors=['green', 'red'])
+    my_circle = plt.Circle((0,0), 0.7, color='white')
+    fig = plt.gcf()
+    fig.gca().add_artist(my_circle)
     
     # Result message
     if pred == 'Zone 1':
         result_string = "Good news \U0001F973\U0001F973\U0001F973! You are likely to park at a lot near to your apartment block!"
-        proba = round(pred_proba[1], 2)
+        proba = round(pred_proba[1], 3)
     else:
         result_string = '\U0001F974\U0001F974\U0001F974 Oops. Looks like all nearest parking lots might have been taken up. Come home earlier next time!'
-        proba = round(pred_proba[0], 2)
+        proba = round(pred_proba[0], 3)
     st.markdown(result_string)
 
     # Expander to view prediction results
     with st.expander('View prediction results'):
-        st.markdown(f"<h3 style='text-align: center; color: black;'>{pred} (with {proba} probability)</h1>", unsafe_allow_html=True)
-        st.plotly_chart(pred_fig_pie)
+        s1, s2 = st.columns([1, 3])
+        s1.markdown(f"<h3 style='text-align: center; color: black;'>{pred} (with {proba} probability)</h1>", unsafe_allow_html=True)
+        s2.pyplot(fig)
 
 if __name__ == "__main__":
     main()
